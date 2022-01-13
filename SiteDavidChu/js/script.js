e2a70763498b028d42f1b9a763f69c133e78d2d2
Clément Cahuzac <clement.cahuzac@gmail.com>
2022-01-13 15:59:28 +0100

    avant test menu burger

---
+++ SiteDavidChu/js/script.js
@@ -0,0 +1,30 @@
+
+document.addEventListener("DOMContentLoaded", 
+    function (event) {
+        document.querySelector("button").addEventListener("click", function () {
+            var self = this;
+            var name = "";
+
+            $ajaxUtils.sendGetRequest("/data/nam.txt", function (request) {
+                self.name = request.responseText;
+            });
+
+            document.querySelector("#content").innerHTML = "<h2>Hello" + self.name + "!";
+        });
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
